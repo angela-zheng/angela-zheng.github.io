b77a5a0a8c380c1091659e96072c64ce05084049
angela-zheng <angela.zhengg@gmail.com>
2018-09-19 00:18:25 -0400

    first steps

---
+++ Scripts/all.js
@@ -0,0 +1,3 @@
+window.onload = function() {
+document.getElementById("navBar").innerHTML =  "<ul><li><a href='process.html'>PROCESS</a></li><li><a href='work.html'>WORK</a></li><li><a href='resume.html'>RESUME</a></li><li><a href='about.html'>ABOUT</a></li></ul>";
+};
